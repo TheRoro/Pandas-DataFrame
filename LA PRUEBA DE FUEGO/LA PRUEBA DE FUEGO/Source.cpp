c2590da9aaf294b50ebc4b5ca4827282eef3009b
vilaron <49617031+vilaron@users.noreply.github.com>
2019-06-05 18:08:43 -0500

    Se creó la clase Dataframe

--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <conio.h>
+#include "DataFrame.h"
 using namespace std;
 
 int main() {
@@ -8,5 +9,6 @@ int main() {
 	cout << "holoa";
 	cout << "mascotita";
 	cout << "eres gil";
+	_getch();
 	return 0;
 }
