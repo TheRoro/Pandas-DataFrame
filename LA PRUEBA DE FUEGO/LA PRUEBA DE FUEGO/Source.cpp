c9de0855cb5c8757194b08922cba094b64cdb894
Rodrigo Ramirez <rodrigo8rb@htomail.com>
2019-06-06 07:46:11 -0500

    Inicializadas las clases y menu basico

--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <conio.h>
+
 #include "DataFrame.h"
 #include "Controladora.h"
+#include "Column.h"
 #include "Row.h"
-#include"Column.h"
 
 using namespace std;
+using namespace System;
 
 int main() {	
 
-	cout << "--------------PANDAS-------------" << endl;
+	DataFrame    dataFrame = DataFrame();
+	Controladora control   = Controladora();
+	Column       column    = Column();
+	Row          row       = Row();
+
+	system("Color 0F");
+	
+
+	cout << "------------------------------PANDAS---------------------------" << endl;
 	cout << "By: Los Papi Pros && Vila" << endl;
+	cout << "A. Importación de Datos " << endl;
+	cout << "B. Indexado de datos por columnas" << endl;
+	cout << "C. Seleccion de datos por columnas" << endl;
+	cout << "D. Flitrado de datos por columas" << endl;
+	cout << "E. Ordenamiento de datos por columnas" << endl;
+	cout << "F. Exportación de datos a archivos planos con diferente formato" << endl;
+
 
 
 
