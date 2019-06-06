c8700f7f9dcb9d5444ade93ca38b28da29391f12
Rodrigo Ramirez <rodrigo8rb@htomail.com>
2019-06-06 10:41:25 -0500

    Funcion menu en la clase controadora

--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Controladora.h
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Controladora.h
@@ -1,11 +1,23 @@
 #pragma once
 
+using namespace std;
+
 class Controladora {
 
 public:
 	Controladora() {
 
 	}
+	void menu() {
 
+		cout << "------------------------------PANDAS---------------------------" << endl;
+		cout << "By: Los Papi Pros && Vila" << endl;
+		cout << "A. Importación de Datos " << endl;
+		cout << "B. Indexado de datos por columnas" << endl;
+		cout << "C. Seleccion de datos por columnas" << endl;
+		cout << "D. Flitrado de datos por columas" << endl;
+		cout << "E. Ordenamiento de datos por columnas" << endl;
+		cout << "F. Exportación de datos a archivos planos con diferente formato" << endl;
+	}
 
 };
--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Source.cpp
@@ -18,17 +18,7 @@ int main() {
 
 	system("Color 0F");
 	
-
-	cout << "------------------------------PANDAS---------------------------" << endl;
-	cout << "By: Los Papi Pros && Vila" << endl;
-	cout << "A. Importación de Datos " << endl;
-	cout << "B. Indexado de datos por columnas" << endl;
-	cout << "C. Seleccion de datos por columnas" << endl;
-	cout << "D. Flitrado de datos por columas" << endl;
-	cout << "E. Ordenamiento de datos por columnas" << endl;
-	cout << "F. Exportación de datos a archivos planos con diferente formato" << endl;
-
-
+	control.menu();
 
 
 	_getch();
