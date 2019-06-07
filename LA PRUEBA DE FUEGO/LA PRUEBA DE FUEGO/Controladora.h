644a145a0fabcad968fd0d9db45d26daf93a4db0
Rodrigo Ramirez <rodrigo8rb@htomail.com>
2019-06-07 00:05:02 -0500

    FUNCIONES IMPORT Y EXPORT DATA IMPLEMENTADAS EN SU FORMA BÁSICA.

--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Controladora.h
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Controladora.h
@@ -5,43 +5,48 @@ using namespace std;
 
 class Controladora {
 	char option;
+	DataFrame dataFrame = DataFrame();
+	bool continuar = true;
 public:
-	Controladora() {
-		DataFrame dataFrame = DataFrame();
-	}
+	Controladora() { }
+
 	void menu() {
 
-		cout << "------------------------------PANDAS--------------------------" << endl;
-		cout << "By: Los Papi Pros && Vila" << endl;
-		cout << "A. Importación de Datos " << endl;
-		cout << "B. Indexado de datos por columnas" << endl;
-		cout << "C. Seleccion de datos por columnas" << endl;
-		cout << "D. Flitrado de datos por columas" << endl;
-		cout << "E. Ordenamiento de datos por columnas" << endl;
-		cout << "F. Exportación de datos a archivos planos con diferente formato" << endl;
-		cin >> option;
+		do {
+			cout << "------------------------------PANDAS--------------------------" << endl;
+			cout << "By: Los Papi Pros && Vila" << endl;
+			cout << "A. Importación de Datos " << endl;
+			cout << "B. Indexado de datos por columnas" << endl;
+			cout << "C. Seleccion de datos por columnas" << endl;
+			cout << "D. Flitrado de datos por columas" << endl;
+			cout << "E. Ordenamiento de datos por columnas" << endl;
+			cout << "F. Exportación de datos a archivos planos con diferente formato" << endl;
+			cout << "G. Salir del Programa" << endl;
+			cin >> option;
 
-		switch (option) {
-		case 'A':
-		
-			break;
-		case 'B':
-			
-			break;
-		case 'C':
-			
-			break;
-		case 'D':
-			
-			break;
-		case 'E':
-			
-			break;
-		case 'F':
-			
-			break;
-		}
-
+			switch (option) {
+			case 'A':
+				dataFrame.importData();
+				break;
+			case 'B':
+				dataFrame.indexData();
+				break;
+			case 'C':
+				dataFrame.selectData();
+				break;
+			case 'D':
+				dataFrame.filterData();
+				break;
+			case 'E':
+				dataFrame.sortData();
+				break;
+			case 'F':
+				dataFrame.exportData();
+				break;
+			case 'G':
+				continuar = false;
+				break;
+			}
+		} while (continuar);
 	}
-
 };
