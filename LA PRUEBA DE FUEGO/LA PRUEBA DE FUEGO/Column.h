d265a4a3aa55b7372ea7e71ff1c6dd48e66bf730
vilaron <49617031+vilaron@users.noreply.github.com>
2019-06-18 17:41:20 -0500

    Se implemento la función encontrar tipo en la clase Column

--- LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Column.h
+++ LA PRUEBA DE FUEGO/LA PRUEBA DE FUEGO/Column.h
@@ -8,12 +8,11 @@ using namespace std;
 class Column {
 
 	vector <Row> rows;
+	string tipo;
 
 public:
 	Column() {
 		string nombre;
-
-
 	}
 	string getDataAt_j(int j) {
 		
@@ -22,5 +21,19 @@ public:
 	void add(Row r) {
 		rows.push_back(r);
 	}
-
+	int FindType(string j) {
+		if ((j[0] >= 65 && j[0] <= 90) || (j[0] >= 97 && j[0] <= 122)) {
+			return 0; //retorna 0 si es string
+		}
+		else {
+			double aux = stod(j);
+			int aux2 = int(aux);
+			if (aux != (double)aux2) {
+				return -1; //retorna -1 si es double
+			}
+			else {
+				return 1;//retorna 1 si es int 
+			}
+		}
+	}
 };
